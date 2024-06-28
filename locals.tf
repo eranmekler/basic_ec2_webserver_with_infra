118f66df0600e0305023e0db3e248b09244fff7b
eran <meklereran@gmail.com>
2024-06-28 18:40:13 +0300

    formmated terraform files

--- locals.tf
+++ locals.tf
@@ -1,10 +1,10 @@
 locals {
-  ami          = "ami-0551ce4d67096d606"
-  machine_type = "t2.micro"
-  sg_name      = "public_webserver"
-  vpc_name     = "blog'_vpc"
-  region       = "eu-west-1"
-  az           = "eu-west-1a"
-  docker_image = "eranmekler/erans_blog:v1.0"
+  ami                   = "ami-0551ce4d67096d606"
+  machine_type          = "t2.micro"
+  sg_name               = "public_webserver"
+  vpc_name              = "blog'_vpc"
+  region                = "eu-west-1"
+  az                    = "eu-west-1a"
+  docker_image          = "eranmekler/erans_blog:v1.0"
   docker_container_name = "erans_blog"
 }
